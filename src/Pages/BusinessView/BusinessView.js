02b686c8978e8fa7f057670d0f009b7c7568fdb8
Travenedu <82552048+Travenedu@users.noreply.github.com>
2024-04-08 02:16:16 -0400

    created grid for business and category view

--- src/Pages/BusinessView/BusinessView.js
+++ src/Pages/BusinessView/BusinessView.js
@@ -1,29 +1,17 @@
 import React, { useState, useEffect, FormEvent } from "react";
-import { Button, Flex, Heading, Text, TextField, View,
-   WithAuthenticatorProps, withAuthenticator, Card, Image, 
-   Badge, useTheme, Collection, Divider} from "@aws-amplify/ui-react";
+import { Button, Flex, Heading, View, Card, Image, 
+   Badge, Collection, Divider} from "@aws-amplify/ui-react";
 
 
 import "@aws-amplify/ui-react/styles.css";
 import { listBusinesses } from "../../graphql/queries";
-import { Amplify } from "aws-amplify";
+
 import { generateClient } from "aws-amplify/api";
 
 const client = generateClient();
 
 const BusinessView = ({ match }) => {
-  const [showModal, setShowModal] = useState(false);
   const [businesses, setBusiness] = useState([]);
-
-  const handleShowModal = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  //@@@@@@@@@@@@@@
 
   useEffect(() => {
     fetchBusiness();
@@ -36,35 +24,14 @@ const BusinessView = ({ match }) => {
     console.log("Retrieved businesses", businessFromAPI);
   }
 
-  // const items = [
-  //   {
-  //     title: 'Milford - Room #1',
-  //     badges: ['Waterfront', 'Verified'],
-  //   },
-  //   {
-  //     title: 'Milford - Room #2',
-  //     badges: ['Mountain', 'Verified'],
-  //   },
-  // ];
   return (
-    // <section className="main container section">
-
-    //   <div className="ssecTitle">
-    //     <h3 className="title">
-    //       Featured Businesses
-    //     </h3>
-    //   </div>
-
-    //   <div className="secContent grid">
-    //     Data.map(())
-
-    //   </div>
-    // </section>
     
     <Collection
       items={businesses}
-      type="list"
-      direction="row"
+      type="grid"
+      templateColumns="22fr 22fr 22fr 22fr 22fr"
+      templateRows="22rem 22rem 22rem 22rem 22rem"
+      //direction="row"
       gap="20px"
       wrap="nowrap"
     >
@@ -72,7 +39,7 @@ const BusinessView = ({ match }) => {
         <Card
           key={index}
           borderRadius="medium"
-          maxWidth="20rem"
+          width="300px" // Set fixed width for the card
           variation="outlined"
         >
           <Image
@@ -80,19 +47,19 @@ const BusinessView = ({ match }) => {
             alt="Glittering stream with old log, snowy mountain peaks tower over a green field."
           />
           <View padding="xs">
-            <Flex>
-                <Badge
-                  
-                  backgroundColor={
-                    item.category === 'Waterfront' ? 'blue.40' 
-                    : item.category === 'Mountain' ? 'green.40' : 'yellow.40'}
-                >
-                  {item.category}
-                </Badge>
+            <Flex justifyContent="center">
+              <Badge
+                backgroundColor={'yellow.40'}
+              >
+                {item.category}
+              </Badge>
             </Flex>
             <Divider padding="xs" />
             <Heading padding="medium">{item.name}</Heading>
-            <Heading padding="medium">{item.description}</Heading>
+            {/* Apply text-overflow ellipsis to the description */}
+            <p style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
+              {item.description}
+            </p>
             <Button variation="primary" isFullWidth>
               Details
             </Button>
