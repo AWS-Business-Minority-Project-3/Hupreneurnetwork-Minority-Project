aee76dcce675af5841d6fd0ae336dc4e2a17ffb8
yasminsenior02 <75453235+yasminsenior02@users.noreply.github.com>
2024-04-11 21:37:41 -0400

    Update CategoryView.js

--- src/Pages/CategoryView/CategoryView.js
+++ src/Pages/CategoryView/CategoryView.js
@@ -1,42 +1,30 @@
-// CategoriesPage.jsx
+//           </Card>
+//         </Button>
+//       )}
+//     </Collection>
+//   );
+// }
+
+// export default CategoriesView;
+
 import React from 'react';
-import { Button, Flex, Heading, View, Card, Image, 
-  Badge, Collection, Divider} from "@aws-amplify/ui-react";
+import { Button, Card, Heading } from "@aws-amplify/ui-react";
 import categoriesData from './CategoryView.json'; // Import categories data
+import "./CategoryView.css";
 
 function CategoriesView() {
-
-  const items = categoriesData.map(category => ({
-    title: category.name,
-  }));
-
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-    <Collection
-      items={items}
-      type="grid"
-      templateColumns="12fr 12fr 12fr 12fr 12fr"
-      templateRows="12rem 12rem 12rem 12rem 12rem"
-      gap="5px"
-      wrap="nowrap"
-      background="none"
-    >
-      {(item, index) => (
-        <Button key={index} variation="primary" isFullWidth>
-          <Card
-            borderRadius="medium"
-            maxWidth="20rem"
-            variation="outlined"
-            style={{ backgroundColor: 'white', border: '1px solid grey' }}
-          >
-            <View padding="xs">
-              <Heading padding="medium">{item.title}</Heading>
-            </View>
+    <div className="collection">
+      {categoriesData.map((category, index) => (
+        <Button key={index} variation="primary" isFullWidth className='button'>
+          <Card className="card">
+            <div className="content">
+              <Heading className="heading">{category.name}</Heading>
+            </div>
           </Card>
         </Button>
-      )}
-    </Collection>
-  </div>
+      ))}
+    </div>
   );
 }
 
